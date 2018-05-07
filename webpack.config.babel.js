ce2764f22e0a84dbec2b1cf6a578e9d08faf571e
ggolikov <gr.golikov@gmail.com>
2018-05-07 12:08:14 +0300

    Update packages

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,37 +1,76 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+import merge from 'webpack-merge';
 import path from 'path';
 import paths from './config';
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: path.join(paths.src, 'index.html'),
-    name: 'index.html',
-    inject: 'body'
+
+const extractSass = new ExtractTextPlugin({
+    filename: "./css/[name].css"
 });
 
-export default {
-    entry: [
-        __dirname + '/index.js'
-    ],
-    devtool: "cheap-inline-module-source-map",
-    devServer: {
-        historyApiFallback: true,
-    },
-    module: {
-        loaders: [
-            { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
-            { test: /\.css$/, exclude: /node_modules/, loader: 'style-loader!css-loader' },
-            { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/, exclude: /node_modules/, loader: "file-loader?name=[name].[ext]" }
+const common = {
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '.sass', 'scss'],
+        modules: [
+            'node_modules'
         ]
     },
-    output: {
-        path: paths.public,
-        filename: 'bundle.js'
-    },
-    plugins: [
-        HTMLWebpackPluginConfig,
-        new CopyWebpackPlugin([
-            { from: path.join(paths.src, 'css/main.css'), to: path.join(paths.public, 'css/main.css') }
-        ])
-    ]
+    devtool: "cheap-inline-module-source-map",
+    module: {
+        rules: [{
+                    test: /\.jsx?$/,
+                    use: [{
+                        loader: "babel-loader"
+                    }],
+                    exclude: /node_modules/
+                }, {
+                    test: /\.(scss|sass|css)$/i,
+                    use: extractSass.extract({
+                        use: [{
+                            loader: "css-loader" // translates CSS into CommonJS
+                        },
+                        'resolve-url-loader',
+                        {
+                            loader: "sass-loader" // compiles Sass to CSS
+                        }],
+                        fallback: "style-loader" // creates style nodes from JS strings
+                    }),
+                    exclude: /node_modules/
+                },  {
+                    test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.woff2$|\.eot$|\.ttf$|\.wav$|\.mp3$/,
+                    use: [{
+                        loader: "file-loader?name=./icons/[name].[ext]"
+                    }],
+                    exclude: /node_modules/
+                }
+        ]
+    }
+}
+module.exports = {};
+module.exports = (env) => {
+    if (env) {
+        console.log('build');
+        return merge({
+            entry: {
+                'index': `${paths.src}/index.js`,
+            },
+            output: {
+                path: paths.public,
+                filename: '[name].js'
+            },
+            plugins: [
+                extractSass,
+                new HtmlWebpackPlugin({
+                    template: path.join(paths.src, 'index.html'),
+                    name: 'index.html',
+                    chunks: ['index'],
+                    inject: 'body'
+                }),
+                new CopyWebpackPlugin([
+                    { from: path.join(paths.src, 'css'), to: path.join(paths.public, 'css') }
+                ])
+            ]
+        }, common);
+    }
 }
